7164f8fadb92a4a4f9791553003c4c94cb8b1e16
trumpcardworld <developer@trumpcardworld.com>
2025-06-30 19:04:29 +0530

    index.js updated

--- admin-panel/src/app/login/page.tsx
+++ admin-panel/src/app/login/page.tsx
@@ -1,5 +1,5 @@
 import LoginForm from '@/forms/login-form';
-import login_bg from '@assets/img/bg/login-bg.jpg';
+import logo from "@assets/img/logo/logo.png";
 import Link from 'next/link';
 
 const LoginPage = () => {
@@ -9,7 +9,7 @@ const LoginPage = () => {
         <div className="pt-[120px] pb-[120px]">
           <div className="grid grid-cols-12 shadow-lg bg-white overflow-hidden rounded-md ">
             <div className="col-span-4 lg:col-span-6 relative h-full hidden lg:block">
-              <div className="data-bg absolute top-0 left-0 w-full h-full bg-cover bg-no-repeat" data-bg="assets/img/bg/login-bg.jpg" style={{backgroundImage:`url(${login_bg.src})`}}></div>
+              <div className="data-bg absolute top-0 left-0 w-full h-full bg-contain bg-no-repeat" data-bg="assets/img/bg/login-bg.jpg" style={{backgroundImage:`url(${logo.src})`, width:'460px'}}></div>
             </div>
             <div className="col-span-12 lg:col-span-6 md:w-[500px] mx-auto my-auto  pt-[50px] py-[60px] px-5 md:px-[60px]">
               <div className="text-center">
--- admin-panel/src/app/register/page.tsx
+++ admin-panel/src/app/register/page.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Link from 'next/link';
-import register_bg from '@assets/img/bg/login-bg.jpg';
+import logo from "@assets/img/logo/logo.png";
 import RegisterForm from '@/forms/register-form';
 
 const RegisterPage = () => {
@@ -10,9 +10,9 @@ const RegisterPage = () => {
         <div className="pt-[120px] pb-[120px]">
           <div className="grid grid-cols-12 shadow-lg bg-white overflow-hidden rounded-md ">
             <div className="col-span-4 lg:col-span-6 relative h-full hidden lg:block">
-              <div className="data-bg absolute top-0 left-0 w-full h-full bg-cover bg-no-repeat" data-bg="assets/img/bg/login-bg.jpg" style={{backgroundImage:`url(${register_bg.src})`}}></div>
+              <div className="data-bg absolute top-0 left-0 w-full h-full bg-contain bg-no-repeat" data-bg="assets/img/bg/login-bg.jpg" style={{backgroundImage:`url(${logo.src})`}}></div>
             </div>
-            <div className="col-span-12 lg:col-span-6 md:w-[500px] mx-auto my-auto  pt-[50px] py-[60px] px-5 md:px-[60px]">
+            <div className="col-span-12 lg:col-span-6 md:w-[500px] mx-auto my-auto  pt-[20px] py-[45px] px-5 md:px-[60px]">
               <div className="text-center">
                 <h4 className="text-[24px] mb-1">Register Now.</h4>
                 <p>Already have an account?  <span> 
